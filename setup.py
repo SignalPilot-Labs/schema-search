69e3de1fddda64bdc575bcc85633d2fda41522d3
Adib Hasan <nzxcvb5@gmail.com>
2025-12-18 16:55:32 -0500

    Bump version to v1.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="schema-search",
-    version="0.1.10",
+    version="1.0.11",
     description="Natural language database schema search with graph-aware semantic retrieval",
     author="Adib Hasan",
     long_description=open("README.md").read(),
